e22b7f9e36105c4ceb0c2ad8d9d5a60f8f9d3bab
christopher rowland <christopher.o.rowland@colorado.edu>
2024-11-28 01:30:19 -0700

    Assignment3-part2--updated-to-new-crosscompiler.

--- finder-app/manual-linux.sh
+++ finder-app/manual-linux.sh
@@ -23,23 +23,19 @@ else
 	echo "Using passed directory ${OUTDIR} for output"
 fi
 
-#if [ $full_run == 1 ]; then
 mkdir -p ${OUTDIR}
-#sudo chmod 777 ${OUTDIR}
 cd "$OUTDIR"
-echo "moved to outdir"
-pwd
 if [ ! -d "${OUTDIR}/linux-stable" ]; then
-    #Clone only if the repository does not exist.
+    	#Clone only if the repository does not exist.
 	echo "CLONING GIT LINUX STABLE VERSION ${KERNEL_VERSION} IN ${OUTDIR}"
 	git clone ${KERNEL_REPO} --depth 1 --single-branch --branch ${KERNEL_VERSION}
 fi
+
 if [ ! -e ${OUTDIR}/linux-stable/arch/${ARCH}/boot/Image ]; then
     cd ${OUTDIR}/linux-stable
     echo "Checking out version ${KERNEL_VERSION}"
     git checkout ${KERNEL_VERSION}
     echo "making the kernel"
-    #cd ./arch/${ARCH} || echo "cannot find the arch dir!"
     echo "runing mrproper to clean..."
     make mrproper  
     echo "making defconfig"
@@ -51,44 +47,48 @@ if [ ! -e ${OUTDIR}/linux-stable/arch/${ARCH}/boot/Image ]; then
     echo "making dev tree"
     make ARCH=$ARCH CROSS_COMPILE=${CROSS_COMPILE} dtbs
     echo "completed building kernel"
-    # TODO: Add your kernel build steps here
+    
 fi
+
 echo "Adding the Image in outdir"
 cp ${OUTDIR}/linux-stable/arch/${ARCH}/boot/Image ${OUTDIR}
 echo "Creating the staging directory for the root filesystem"
 cd "$OUTDIR"
+
 if [ -d "${OUTDIR}/rootfs" ]
 then
     echo "Deleting rootfs directory at ${OUTDIR}/rootfs and starting over"
     sudo rm -rf ${OUTDIR}/rootfs
 fi
+
 mkdir ${OUTDIR}/rootfs || echo "ROOTFS already exists"
+
 # TODO: Create necessary base directories
 list="bin,dev,etc,home,lib,lib64,proc,sbin,sys,tmp,usr,var,./usr/bin,./usr/lib,./usr/sbin,./var/log"
 IFS=','
 for dir in $list
-do
-[ -d ${OUTDIR}/rootfs/$dir ] || mkdir -p ${OUTDIR}/rootfs/$dir
-echo "made dir:"
-ls -lah ${OUTDIR}/rootfs/$dir || echo "failed to make dir ${OUTDIR}/rootfs/$dir"   
-done
+	do
+		[ -d ${OUTDIR}/rootfs/$dir ] || mkdir -p ${OUTDIR}/rootfs/$dir
+		echo "made dir:"
+		ls -lah ${OUTDIR}/rootfs/$dir || echo "failed to make dir ${OUTDIR}/rootfs/$dir"   
+	done
 
 if [ ! -d "${OUTDIR}/busybox" ]
-then
+	then
+	git clone git://busybox.net/busybox.git
+	cd ${OUTDIR}/busybox
+	git checkout ${BUSYBOX_VERSION}
 
-git clone git://busybox.net/busybox.git
-    cd ${OUTDIR}/busybox
-    git checkout ${BUSYBOX_VERSION}
-    # TODO:  Configure busybox
-else
-    cd busybox
+	else
+    	cd ${OUTDIR}/busybox
 fi
+
 make distclean
 make defconfig
 make ARCH=${ARCH} CROSS_COMPILE=${CROSS_COMPILE}
 make CONFIG_PREFIX=${OUTDIR}/rootfs ARCH=${ARCH} CROSS_COMPILE=${CROSS_COMPILE} install
-# TODO: Make and install busybox
-# TODO: Add library dependencies to rootfs
+	# TODO: Make and install busybox
+	# TODO: Add library dependencies to rootfs
 echo "Library dependencies"
 dep_names="libm.so.6,libresolv.so.2,libc.so.6"
 lib_dep_names="ld-linux-aarch64.so.1"
@@ -123,17 +123,15 @@ cp writer ${OUTDIR}/rootfs/home/
 echo "HERE IS CHMOD"
 sudo chmod 755 ${OUTDIR}/rootfs/home/writer
 echo "HERE IS RESULT:""$(ls -ll ${OUTDIR}/rootfs/home/writer)"
-#popd
+
 # TODO: Copy the finder related scripts and executables to the /home directory
 # on the target rootfs
+
 mkdir -p ${OUTDIR}/rootfs/home/conf
 sudo chmod -R 755 ${OUTDIR}/rootfs || echo "issues with chmod -R 755 ${OUTDIR}/rootfs"
-sudo chown -R root:root ${OUTDIR}/rootfs || echo "issues with chown -R root:root ${OUTDIR}/rootfs"
-#fi #full_run
+#sudo chown -R root:root ${OUTDIR}/rootfs || echo "issues with chown -R root:root ${OUTDIR}/rootfs"
 echo "MY PWD IS "$(pwd)""
 cd $FINDER_APP_DIR
-#echo "/bin/sh" > ${OUTDIR}/rootfs/init
-#chmod 755 ${OUTDIR}/rootfs/init
 mkdir -p ${OUTDIR}/rootfs/home/conf
 cp $FINDER_APP_DIR/finder.sh ${OUTDIR}/rootfs/home/
 cp $FINDER_APP_DIR/autorun-qemu.sh ${OUTDIR}/rootfs/home/
@@ -148,10 +146,10 @@ sudo chmod +x ${OUTDIR}/rootfs/home/finder-test.sh
 sed -i 's;../conf/;conf/;g' ${OUTDIR}/rootfs/home/finder-test.sh
 sed -i 's;../conf/;conf/;g' ${OUTDIR}/rootfs/home/finder.sh
 # TODO: Chown the root directory
-sudo chown -R root ${OUTDIR}/rootfs
+#sudo chown -R root ${OUTDIR}/rootfs
 #sudo chmod -R 777 ${OUTDIR}/rootfs  #This is insecure. But security is not one of our objectives here.
 cd ${OUTDIR}/rootfs
-chown -R root:root ./* 
+#chown -R root:root ./* 
 find . | cpio -H newc -ov --owner=root:root > ${OUTDIR}/initramfs.cpio
 gzip -f ${OUTDIR}/initramfs.cpio
 # TODO: Create initramfs.cpio.gz
